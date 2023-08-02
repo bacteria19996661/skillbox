d5d82fdd102d7d5b43b7d79e558ff8073f05fc7a
Lika Bacteria <bacteria@mail.ru>
2023-08-02 14:22:10 +0300

    mod 4, task 7: upgrade

--- part_2/2_04/2_04_task_7.py
+++ part_2/2_04/2_04_task_7.py
@@ -12,6 +12,13 @@
 # Переменные и функции имеют значимые имена, не только a, b, c, d.
 
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
-result = [z for x in range(len(nice_list)) for y in range(len(nice_list[x])) for z in nice_list[x][y]]
 
+# мое решение
+result = [z for x in range(len(nice_list)) for y in range(len(nice_list[x])) for z in nice_list[x][y]]
 print(result)
+
+# решение skillbox
+# output = [digit for each_list in nice_list for each_list_2 in each_list for digit in each_list_2]
+# print(output)
+
+
