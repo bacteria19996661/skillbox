21590ace0e64ebf6098ff17728384b43c8808b7a
Lika <bacteria@mail.ru>
2024-01-13 09:26:50 +0300

    mod 19, bot: code refactoring

--- part_2/my_telebot/loader.py
+++ part_2/my_telebot/loader.py
@@ -8,17 +8,12 @@ from config_data.config import BOT_TOKEN
     StateMemoryStorage - это хранилище состояний, предоставляемое библиотекой telebot,
     которое позволяет боту отслеживать состояния пользователей или чатов.
     Он используется для хранения информации о текущем состоянии чата или пользователя внутри бота.
-    Это может быть полезно, когда вам нужно помнить контекст взаимодействия пользователя
-    с ботом для обработки запросов.
+    Полезно, когда нужно помнить контекст взаимодействия пользователя с ботом для обработки запросов.
     
-    telebot.TeleBot(BOT_TOKEN, state_storage=storage) -
-    Создание экземпляра класса telebot.TeleBot с передачей токена BOT_TOKEN для инициализации бота.
+    TeleBot(BOT_TOKEN, state_storage=storage) - создание экземпляра класса 
+    telebot.TeleBot с передачей токена BOT_TOKEN для инициализации бота.
     Параметр state_storage=storage используется для указания хранилища состояний,
     созданного ранее, для отслеживания состояний пользователей или чатов.
 """
-
-state_storage = StateMemoryStorage()
-
-
 storage = StateMemoryStorage()
 bot = TeleBot(token=BOT_TOKEN, state_storage=storage)
